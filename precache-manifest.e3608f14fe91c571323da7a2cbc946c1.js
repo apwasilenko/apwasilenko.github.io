e741e107ab384852541af211b835fa7ccaede018
Anton Vasilenko <67705689+apwasilenko@users.noreply.github.com>
2022-09-27 20:54:08 +0700

    Add files via upload
    Добавил из

---
+++ precache-manifest.e3608f14fe91c571323da7a2cbc946c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6575f178e900510e51d8",
+    "url": "/static/css/main.f0ffdf27.chunk.css"
+  },
+  {
+    "revision": "6575f178e900510e51d8",
+    "url": "/static/js/main.6575f178.chunk.js"
+  },
+  {
+    "revision": "9d99ffc361642ab616e2",
+    "url": "/static/js/1.9d99ffc3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31b7f6bfdb171a2c8ee1fc6e986b4502",
+    "url": "/index.html"
+  }
+];
