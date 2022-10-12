885809e112210991e18c299b61e983cfd8f850a6
APWasilenko <APWasilenko@gmail.com>
2022-10-12 17:58:59 +0700

    12-10-22

---
+++ precache-manifest.38a5414f70c308a9c92bf05b3561a28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89da0c9f571c39e6441",
+    "url": "/static/css/main.80d6dc90.chunk.css"
+  },
+  {
+    "revision": "d89da0c9f571c39e6441",
+    "url": "/static/js/main.d89da0c9.chunk.js"
+  },
+  {
+    "revision": "5e2a5db1dab2781fc2e2",
+    "url": "/static/js/1.5e2a5db1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25e1ff9d7e09ce2e4fb630f9f08d9e7f",
+    "url": "/index.html"
+  }
+];
